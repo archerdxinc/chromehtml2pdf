8767d14598236ced29448f4b690780d849a2f1f2
Jody Stephens <jstephens@archerdx.com>
2019-10-06 15:17:17 -0600

    Fixes issue when running as root

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ c.action(function(file){
     // Get the page to create the PDF.
     (async () => {
 	try{
-	    var launchConfig = {};
+            var launchConfig = {args: ['--no-sandbox', '--disable-setuid-sandbox'] };
 	    if(c.executablePath){
 		console.log('Using chrome executable: '+c.executablePath);
 		launchConfig.executablePath = c.executablePath;
